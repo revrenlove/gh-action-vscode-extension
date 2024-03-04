c87932c95bd8eaba116d60bb5502b052835fcaf7
revrenlove <elrd.dev@gmail.com>
2024-03-03 18:15:00 -0600

    printing output

--- .github/workflows/actions/js-test/index.js
+++ .github/workflows/actions/js-test/index.js
@@ -3,7 +3,9 @@ const github = require("@actions/github");
 
 const event = require(process.env.GITHUB_EVENT_PATH);
 
-const commit = event.commits[event.commits.length - 1];
+console.log(JSON.stringify(event, null, 4));
 
-console.log(commit.message);
-console.log(commit.body);
+// const commit = event.commits[event.commits.length - 1];
+
+// console.log(commit.message);
+// console.log(commit.body);
