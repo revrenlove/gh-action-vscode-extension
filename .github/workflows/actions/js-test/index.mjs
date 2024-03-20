12feeb73c98e94b8f78f7453a841620a4c3e35f1
revrenlove <elrd.dev@gmail.com>
2024-03-20 15:56:39 -0500

    even MORE tweaking

--- .github/workflows/actions/js-test/index.mjs
+++ .github/workflows/actions/js-test/index.mjs
@@ -4,16 +4,9 @@ import * as github from "@actions/github";
 
 console.log("Starting");
 
-console.log(github.context.payload);
-
-console.log(JSON.stringify(event, null, 4));
+console.log(JSON.stringify(github.context.payload));
 
 console.log(core.ExitCode.Success.toLocaleString());
 
 const idToken = await core.getIDToken();
 console.log(idToken);
-
-// const commit = event.commits[event.commits.length - 1];
-
-// console.log(commit.message);
-// console.log(commit.body);
