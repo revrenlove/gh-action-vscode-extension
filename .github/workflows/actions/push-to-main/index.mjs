cf02b9f4f4cf132bde12ecaadfdde43cdeca4ccd
revrenlove <58348549+revrenlove@users.noreply.github.com>
2024-03-20 17:48:54 -0500

    console log PR object (#2)
    * console log PR object
    
    * printing whole payload
    
    ---------
    
    Co-authored-by: revrenlove <elrd.dev@gmail.com>

--- .github/workflows/actions/push-to-main/index.mjs
+++ .github/workflows/actions/push-to-main/index.mjs
@@ -1,10 +1,21 @@
 // import * as core from "@actions/core";
 import * as github from "@actions/github";
 
+const print = (msg) => {
+    if (typeof msg === "object") {
+        msg = JSON.stringify(msg, null, 4);
+    }
+
+    console.log(msg);
+};
+
 var payload = github.context.payload;
 
-var payloadJson = JSON.stringify(payload, null, 4);
+print(payload);
 
-// Adding random comment
+if (!payload.pull_request) {
+    print("No pull request object.");
+    return;
+}
 
-console.log(payloadJson);
+print(payload.pull_request);
