86a0c774bd89220955728b980e6089b3a3679f32
revrenlove <58348549+revrenlove@users.noreply.github.com>
2024-03-21 23:13:11 -0500

    trying to read the package json (#13)
    Co-authored-by: revrenlove <elrd.dev@gmail.com>

--- .github/workflows/actions/pull-request-closed/index.mjs
+++ .github/workflows/actions/pull-request-closed/index.mjs
@@ -1,54 +1,6 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
-
-// const print = (msg) => {
-//     if (typeof msg === "object") {
-//         msg = JSON.stringify(msg, null, 4);
-//     }
-
-//     console.log(msg);
-// };
-
-print("pull-request-closed workflow starting...");
-
-const payload = github.context.payload;
-
-print(payload);
-
-// MAIN CODE GOES HERE
-const execute = () => {
-    if (!payload.pull_request.merged) {
-        print("PR was closed without merging. Exiting.");
-
-        return;
-    }
-
-    payload.pull_request.labels.forEach((label) => {
-        print(label.name);
-    });
-
-    // []
-    const labelNames = payload.pull_request.labels.map((label) => label.name);
-};
-
-// TODO: JE - Validate shit...
-
-// Shit like this should go in a validate method...
-
-// if (!payload.pull_request.merged) {
-//     print("PR was closed without merging. Exiting.");
-
-//     return;
-// }
-
-print("Starting execute method...");
-execute();
-
-// TODO: Bump version
-
-// TODO: Tag release
-
-// TODO: publish
+import fetch from "node-fetch";
 
 const print = (msg) => {
     if (typeof msg === "object") {
@@ -57,3 +9,75 @@ const print = (msg) => {
 
     console.log(msg);
 };
+
+print("pull-request-closed workflow starting...");
+
+// const payload = github.context.payload;
+
+// print(payload);
+
+class Version {
+    constructor(versionString) {
+        const parts = versionString.split(".");
+
+        this.major = parts[0];
+        this.minor = parts[1];
+        this.patch = parts[2];
+
+        this.toString = () => `${this.major}.${this.minor}.${this.patch}`;
+    }
+}
+
+// Labels:
+//  ===========
+//  major
+//  minor
+//  patch
+//  ===========
+//  release
+//  prerelease
+//  ===========
+//  publish
+
+const bumpMajor = async () => {
+    print("Bumping major...");
+
+    // Get tha package.json file
+    const rawPackageJson = await fetch(
+        "https://raw.githubusercontent.com/revrenlove/gh-action-vscode-extension/main/package.json"
+    );
+
+    console.log(rawPackageJson);
+
+    // Get current version from package json
+
+    // mod package.json
+
+    // commit directly to main
+};
+
+const bumpVersion = (version) => {
+    // mod package.json
+    // commit directly to main
+};
+
+// MAIN CODE GOES HERE
+(() => {
+    // if (!payload.pull_request.merged) {
+    //     print("PR was closed without merging. Exiting.");
+    //     return;
+    // }
+    // payload.pull_request.labels.forEach((label) => {
+    //     print(label.name);
+    // });
+    // // []
+    // const labelNames = payload.pull_request.labels.map((label) => label.name);
+    // TODO: JE - Validate shit...
+    // TODO: Bump version
+    // TODO: Tag release
+    // TODO: publish
+
+    if (payload.pull_request.labels.includes("major")) {
+        bumpMajor();
+    }
+})();
