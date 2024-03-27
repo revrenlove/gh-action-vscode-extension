6b41be62b07a895a1342da1c0422af8fb4767073
revrenlove <elrd.dev@gmail.com>
2024-03-27 00:24:04 -0500

    printing payload

--- .github/workflows/actions/pull-request-closed/index.mjs
+++ .github/workflows/actions/pull-request-closed/index.mjs
@@ -18,106 +18,107 @@ print("pull-request-closed workflow starting...");
 
 const payload = github.context.payload;
 
-// print(payload);
+print(payload);
 
-class Version {
-    constructor(versionString) {
-        const parts = versionString.split(".");
+// class Version {
+//     constructor(versionString) {
+//         const parts = versionString.split(".");
 
-        this.major = parts[0];
-        this.minor = parts[1];
-        this.patch = parts[2];
+//         this.major = parts[0];
+//         this.minor = parts[1];
+//         this.patch = parts[2];
 
-        this.toString = () => `${this.major}.${this.minor}.${this.patch}`;
-    }
-}
+//         this.toString = () => `${this.major}.${this.minor}.${this.patch}`;
+//     }
+// }
 
-// Labels:
-//  ===========
-//  major
-//  minor
-//  patch
-//  ===========
-//  release
-//  prerelease
-//  ===========
-//  publish
+// // Labels:
+// //  ===========
+// //  major
+// //  minor
+// //  patch
+// //  ===========
+// //  release
+// //  prerelease
+// //  ===========
+// //  publish
 
-async function bumpMajor() {
-    // const packageJson = await getPackageJson();
-    // const version = new Version(packageJson.version);
-    // print(`Version before anything: ${version}`);
-    // version.major++;
-    // packageJson.version = version.toString();
-    // await updatePackageJson(packageJson);
+// async function bumpMajor() {
+//     // const packageJson = await getPackageJson();
+//     // const version = new Version(packageJson.version);
+//     // print(`Version before anything: ${version}`);
+//     // version.major++;
+//     // packageJson.version = version.toString();
+//     // await updatePackageJson(packageJson);
 
-    await updateVersionAndCommitPackageJsonChanges();
-}
+//     await updateVersionAndCommitPackageJsonChanges();
+// }
 
-async function updateVersionAndCommitPackageJsonChanges(labelNames) {
-    if (!labelNames) return;
+// // TODO: Separate logic???
+// async function updateVersionAndCommitPackageJsonChanges(labelNames) {
+//     if (!labelNames) return;
 
-    const packageJson = await getPackageJson();
+//     const packageJson = await getPackageJson();
 
-    const version = new Version(packageJson.version);
+//     const version = new Version(packageJson.version);
 
-    print(`Version before anything: ${version}`);
+//     print(`Version before anything: ${version}`);
 
-    if (labelNames.includes("major")) {
-        version.major++;
-        version.minor = 0;
-        version.patch = 0;
-    } else if (labelNames.includes("minor")) {
-        version.minor++;
-        version.patch = 0;
-    } else if (labelNames.includes("patch")) {
-        version.patch++;
-    }
+//     if (labelNames.includes("major")) {
+//         version.major++;
+//         version.minor = 0;
+//         version.patch = 0;
+//     } else if (labelNames.includes("minor")) {
+//         version.minor++;
+//         version.patch = 0;
+//     } else if (labelNames.includes("patch")) {
+//         version.patch++;
+//     }
 
-    packageJson.version = version.toString();
+//     packageJson.version = version.toString();
 
-    await updatePackageJson(packageJson);
-}
+//     await updatePackageJson(packageJson);
+// }
 
-const updatePackageJson = async (packageJson) => {
-    const packageJsonAsString = JSON.stringify(packageJson, null, 2);
+// const updatePackageJson = async (packageJson) => {
+//     const packageJsonAsString = JSON.stringify(packageJson, null, 2);
 
-    fs.writeFile("./package.json", packageJsonAsString, async () => {
-        print("Fuck callbacks...");
+//     fs.writeFile("./package.json", packageJsonAsString, async () => {
+//         print("Fuck callbacks...");
 
-        await exec.exec(
-            "git config --global user.email",
-            "elrod.dev@gmail.com"
-        );
-        await exec.exec('git config --global user.name "Jim\'s Robot"');
+//         await exec.exec(
+//             "git config --global user.email",
+//             "elrod.dev@gmail.com"
+//         );
+//         await exec.exec('git config --global user.name "Jim\'s Robot"');
 
-        await exec.exec("git add -A");
-        await exec.exec("git commit -m", `"Updating version to ${version}"`);
-        await exec.exec("git push -f");
-    });
-};
+//         await exec.exec("git add -A");
+//         await exec.exec("git commit -m", `"Updating version to ${version}"`);
+//         await exec.exec("git push -f");
+//     });
+// };
 
-const getPackageJson = async () => {
-    const response = await fetch(
-        "https://raw.githubusercontent.com/revrenlove/gh-action-vscode-extension/main/package.json"
-    );
+// const getPackageJson = async () => {
+//     const response = await fetch(
+//         "https://raw.githubusercontent.com/revrenlove/gh-action-vscode-extension/main/package.json"
+//     );
 
-    const packageJson = await response.json();
+//     const packageJson = await response.json();
 
-    return packageJson;
-};
+//     return packageJson;
+// };
 
-// MAIN CODE GOES HERE
-(() => {
-    const labelNames = payload.pull_request.labels.map((label) => label.name);
-    // TODO: JE - Validate shit...
-    // TODO: Bump version
-    // TODO: Tag release
-    // TODO: publish
+// // MAIN CODE GOES HERE
+// (() => {
+//     const labelNames = payload.pull_request.labels.map((label) => label.name);
+//     // TODO: JE - Validate shit...
+//     // TODO: Bump version
+//     // TODO: Tag release
+//     // TODO: publish
 
-    let newVersion = 0;
+//     let newVersion = 0;
 
-    if (labelNames.includes("major")) {
-        bumpMajor();
-    }
-})();
+//     if (labelNames.includes("major")) {
+//         bumpMajor();
+//     }
+// })();
